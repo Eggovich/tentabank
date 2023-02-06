2e9a84ccac87ce5412cc9fe09e5e61f41e08ef63
SimonG11 <simon.gottschalk@hotmail.com>
2023-02-06 12:19:25 +0100

    ändrar hemsidan

--- tentabank-bth/src/pages/index.js
+++ tentabank-bth/src/pages/index.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <h1 className="header">Welcome to Student Central</h1>
-      <p className="subheader">A Place to Learn, Grow, and Succeed</p>
+      <p className="subheader">helli hallååå</p>
     </div>
   );
 };
