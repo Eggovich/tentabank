1f02534db58bb9361a2f530516df80b0953a05b6
Eggovich <103115746+Eggovich@users.noreply.github.com>
2023-02-06 14:36:48 +0100

    revert

--- tentabank-bth/src/App.js
+++ tentabank-bth/src/App.js
@@ -15,7 +15,7 @@ function App() {
 return (
 	<Router>
 	<Navbar />
-	<Routes basename="/">
+	<Routes>
 		<Route exact path='/' element={<Home />} />
 		<Route path='/about' element={<About/>} />
 		<Route path='/upload' element={<Upload/>} />
