177a9ec495b297107cf54d21f625c43f91f40994
Eggovich <103115746+Eggovich@users.noreply.github.com>
2023-02-06 16:19:32 +0100

    img fix 6

--- tentabank-bth/src/components/index.js
+++ tentabank-bth/src/components/index.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
       <Nav>
 	  <div className="logo">
-   		 <img src={"./tentabank-bth/source/components/bilder/logga-tentabank.png"} alt="logo"/> 
+   		 <img src={"./tentabank-bth/src/components/bilder/logga-tentabank.png"} alt="logo"/> 
   		</div>
         <NavMenu>
           <NavLink to="/" activestyle="true">
