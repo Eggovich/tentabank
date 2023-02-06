5b89c3e9e6861ab94a5e10fd51ea933fc604e327
Eggovich <103115746+Eggovich@users.noreply.github.com>
2023-02-06 15:50:46 +0100

    försök att ändra startsida

--- tentabank-bth/src/components/footer.js
+++ tentabank-bth/src/components/footer.js
@@ -8,10 +8,10 @@ return (
         <div className="social"><a href="#"><i className="icon ion-social-instagram"></i></a><a href="#"><i className="icon ion-social-snapchat"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a><a href="#"><i className="icon ion-social-facebook"></i></a></div>
         <ul className="list-inline">
             <li className="list-inline-item"><NavLink to="/">Hem</NavLink></li>
-            <li className="list-inline-item"><a href="/browse">Tentabank</a></li>
-            <li className="list-inline-item"><a href="/upload">Ladda upp</a></li>
-            <li className="list-inline-item"><a href="/about">Om oss</a></li>
-            <li className="list-inline-item"><a href="/profile">Min sida</a></li>
+            <li className="list-inline-item"><NavLink to="/browse">Tentabank</NavLink></li>
+            <li className="list-inline-item"><NavLink to="/upload">Ladda upp</NavLink></li>
+            <li className="list-inline-item"><NavLink to="/about">Om oss</NavLink></li>
+            <li className="list-inline-item"><NavLink to="/profile">Min sida</NavLink></li>
         </ul>
         <p className="copyright">TentaBanken ©</p>
     </footer>
