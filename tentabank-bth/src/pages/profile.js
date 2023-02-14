cd7bed83720b7d6a2268bc3d8f59e43de46ccd40
Eggovich <103115746+Eggovich@users.noreply.github.com>
2023-02-14 16:04:11 +0100

    Profil grej

--- tentabank-bth/src/pages/profile.js
+++ tentabank-bth/src/pages/profile.js
@@ -1,8 +1,17 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React, { useState } from 'react';
 
 const Profile = () => {
+const {user, isAuthenticated} = useAuth0()
 return (
-	<p>Sign in</p>
+	!isAuthenticated? (
+	<p>Sign in</p>) :(
+	<div>
+		<p>{user.nickname}</p>
+		<p>{user.email}</p>
+		<img src={user.picture} alt="logo"/> 
+	</div>
+	)
 );
 };
 
