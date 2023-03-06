c2f79cd337252a68706f1ef278d18b8f119f806b
SimonG11 <simon.gottschalk@hotmail.com>
2023-03-06 14:47:09 +0100

    fotter

--- tentabank-bth/src/components/footer.js
+++ tentabank-bth/src/components/footer.js
@@ -1,22 +1,48 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./Footer.css"; //import your CSS file here
 
 const Footer = () => {
-return (
-    <div className="footer-basic">
-    <footer>
-        <div className="social"><a href="#"><i className="icon ion-social-instagram"></i></a><a href="#"><i className="icon ion-social-snapchat"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a><a href="#"><i className="icon ion-social-facebook"></i></a></div>
-        <ul className="list-inline">
-            <li className="list-inline-item"><NavLink to="/">Hem</NavLink></li>
-            <li className="list-inline-item"><NavLink to="/browse">Tentabank</NavLink></li>
-            <li className="list-inline-item"><NavLink to="/upload">Ladda upp</NavLink></li>
-            <li className="list-inline-item"><NavLink to="/about">Om oss</NavLink></li>
-            <li className="list-inline-item"><NavLink to="/profile">Min sida</NavLink></li>
-        </ul>
-        <p className="copyright">TentaBanken ©</p>
+  return (
+    <footer className="footer-basic">
+      <div className="social">
+        <a href="#">
+          <i className="icon ion-social-instagram"></i>
+        </a>
+        <a href="#">
+          <i className="icon ion-social-snapchat"></i>
+        </a>
+        <a href="#">
+          <i className="icon ion-social-twitter"></i>
+        </a>
+        <a href="#">
+          <i className="icon ion-social-facebook"></i>
+        </a>
+      </div>
+      <ul className="list-inline">
+        <li className="list-inline-item">
+          <NavLink to="/">Hem</NavLink>
+        </li>
+        <li className="list-inline-item">
+          <NavLink to="/browse">Tentabank</NavLink>
+        </li>
+        <li className="list-inline-item">
+          <NavLink to="/upload">Ladda upp</NavLink>
+        </li>
+        <li className="list-inline-item">
+          <NavLink to="/about">Om oss</NavLink>
+        </li>
+        <li className="list-inline-item">
+          <NavLink to="/profile">Min sida</NavLink>
+        </li>
+      </ul>
+      <div className="text-center">
+        <p className="mb-0">
+          TentaBanken ©
+        </p>
+      </div>
     </footer>
-</div>
-);
+  );
 };
 
 export default Footer;
