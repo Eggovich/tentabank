5202cab0163c0a8965c2c69ba82f236fba1264e8
SimonG11 <simon.gottschalk@hotmail.com>
2023-04-08 15:47:07 +0200

    uppdaterade navbar.js och .css

--- tentabank-bth/src/components/navbar.js
+++ tentabank-bth/src/components/navbar.js
@@ -1,46 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import img from "./bilder/logga-tentabank.png";
-import LoginButtom from "./login-button";
-import LogoutButtom from "./logout-button";
-import { useCookies } from "react-cookie";
 import "./navbar.css";
+import img from "./bilder/logga-tentabank.png";
+import LoginButton from "./login-button";
+import LogoutButton from "./logout-button";
+import { useCookies } from "react-cookie";
 
 const Navbar = () => {
   const [cookies] = useCookies(["user"]);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [profileDropdownOpen, setProfileDropdownOpen] = useState(false);
+
+  const handleProfileClick = () => {
+    setProfileDropdownOpen(!profileDropdownOpen);
+  };
+
   return (
     <>
       <nav className="nav">
         <NavLink className="logo" to="/">
           <img src={img} alt="logo" />
         </NavLink>
-        <div className="nav-menu">
-          <NavLink className="nav-link" to="/" activeClassName="active">
-            Hem
-          </NavLink>
-          {cookies.role !== "Reviewer" && (
-            <NavLink className="nav-link" to="/browse" activeClassName="active">
+        <button
+          className="menu-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          ☰
+        </button>
+        <div className={`nav-menu ${menuOpen ? "open" : ""}`}>
+
+          {cookies.role != "Reviewer" && (
+            <NavLink to="/browse" activeClassName="active">
               Tentabank
             </NavLink>
           )}
-          {cookies.role !== "Reviewer" && (
-            <NavLink className="nav-link" to="/upload" activeClassName="active">
+          {cookies.role != "Reviewer" && (
+            <NavLink to="/upload" activeClassName="active">
               Ladda upp
             </NavLink>
           )}
-          {cookies.role === "Reviewer" && (
-            <NavLink className="nav-link" to="/review" activeClassName="active">
+          {cookies.role == "Reviewer" && (
+            <NavLink to="/review" activeClassName="active">
               Granska
             </NavLink>
           )}
-          <NavLink className="nav-link" to="/about" activeClassName="active">
+          <NavLink to="/about" activeClassName="active">
             Om oss
           </NavLink>
-          <NavLink className="nav-link" to="/profile" activeClassName="active">
-            Min sida
-          </NavLink>
-          {!cookies.loggedIn && <LoginButtom />}
-          {cookies.loggedIn && <LogoutButtom />}
+          <div className="profile-icon" onClick={handleProfileClick}>
+            <i className="fas fa-user-circle"></i>
+            {profileDropdownOpen && (
+              <div className="profile-dropdown">
+                <NavLink to="/profile" activeClassName="active">
+                  Min sida
+                </NavLink>
+                <NavLink to="/notifications" activeClassName="active">
+                  Notifications
+                </NavLink>
+                <NavLink to="/settings" activeClassName="active">
+                  Settings
+                </NavLink>
+                {!cookies.loggedIn && <LoginButton />}
+                {cookies.loggedIn && <LogoutButton />}
+              </div>
+            )}
+          </div>
         </div>
       </nav>
     </>
