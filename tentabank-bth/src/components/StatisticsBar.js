0ad5cc6c4055cb9297451c7fd57d4f14e994bc1c
SimonG11 <simon.gottschalk@hotmail.com>
2023-04-13 16:49:08 +0200

    la in ett nytt libary
    npm install react-countup

--- tentabank-bth/src/components/StatisticsBar.js
+++ tentabank-bth/src/components/StatisticsBar.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './StatisticsBar.css';
+import CountUp from 'react-countup';
 
 const StatisticsBar = ({ stats }) => {
   const [statisticsData, setStatisticsData] = useState({});
 
   useEffect(() => {
     const fetchStatisticsData = async () => {
-        const response = await fetch('http://localhost:5000/statistics'); // Replace with your backend URL
-        const data = await response.json();
-        console.log('Statistics Data:', data); // Add this line to log the data
-        setStatisticsData(data);
-      };
-      
+      const response = await fetch('http://localhost:5000/statistics'); // Replace with your backend URL
+      const data = await response.json();
+      setStatisticsData(data);
+    };
 
     fetchStatisticsData();
   }, []);
@@ -20,7 +19,9 @@ const StatisticsBar = ({ stats }) => {
     <div className="statistics-bar">
       {stats.map((stat, index) => (
         <div key={index} className="statistics-item">
-          <h4>{statisticsData[stat.key]}</h4>
+          <h4>
+            <CountUp start={0} end={statisticsData[stat.key] || 0} duration={10} />
+          </h4>
           <p>{stat.text}</p>
         </div>
       ))}
