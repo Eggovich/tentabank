9d6654c99c38b302417ac6948a3171df5e79d38e
Eggovich <103115746+Eggovich@users.noreply.github.com>
2023-04-27 15:29:09 +0200

    browse card

--- tentabank-bth/src/components/Carditemsexam.js
+++ tentabank-bth/src/components/Carditemsexam.js
@@ -8,19 +8,22 @@ import StarRating from './Starrating';
 function CardItemsexam(props) {
   
   return (
-      <div className={styles.master}>        
-          <div className={styles.cover}>{props.courseCode.slice(0,1)}{props.courseCode.slice(1,2).toLowerCase()}</div>
-          <div className={styles.exam_info}>
-            <div className={styles.floor_1}>
-              <div className={styles.exam_text}>{props.courseCode}</div>
-              <div className={styles.exam_text}>Betyg: {props.grade}</div>
-            </div>
-            <div className={styles.floor_2}>
-              <div className={styles.exam_text}>{props.date}</div>
-              <StarRating rating={props.rating} exam_id={props.exam_id}/>
-            </div>
-          </div>
-      </div>
+    <div className={styles.ag_courses_item}>
+      <a href="#" className={styles.ag_courses_item_link}>
+        <div className={styles.ag_courses_item_bg}></div>
+        <div className={styles.ag_courses_item_title}>
+          {props.courseCode.slice(0,1)}{props.courseCode.slice(1,2).toUpperCase()}
+        </div>
+        <div className={styles.ag_courses_item_date_box}>
+          <div className={styles.ag_courses_item_date}>{props.courseCode}</div>
+          <span className={styles.ag_courses_item_date}>
+            {props.date}
+          </span>
+          <div className={styles.ag_courses_item_date}>Betyg: {props.grade}</div>
+          <StarRating rating={props.rating} exam_id={props.exam_id}/>
+        </div>
+      </a>
+    </div>
   );
 }
 
