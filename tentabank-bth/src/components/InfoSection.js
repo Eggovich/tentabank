449c4e128c16c62c8fdc0d53679df52f9c69f8c2
SimonG11 <simon.gottschalk@hotmail.com>
2023-05-30 11:11:13 +0200

    svenksa

--- tentabank-bth/src/components/InfoSection.js
+++ tentabank-bth/src/components/InfoSection.js
@@ -6,21 +6,30 @@ const InfoSection = () => {
   const infoItems = [
     {
       image: checkbox,
-      text: 'Click on "Start Review". Make sure to check that there are no personal information or other inappropriate content included in the exam.'
+      text: 'Klicka på "Börja granska tentor". Kontrollera noga att det inte finns någon personlig information eller annat olämpligt innehåll inkluderat i tentan.'
     },
     {
       image: checkbox,
-      text: 'Decision: If all information is correct and there is nothing inappropriate, check the box confirming the information is correct and then click the "Accept" button.'
+      text: 'Beslut: Om all information är korrekt och det inte finns något olämpligt, kryssa i rutan som bekräftar att informationen är korrekt och klicka sedan på "Godkänn tenta"-knappen.'
     },
     {
       image: checkbox,
-      text: 'If the information is not correct or if there are inappropriate information in the exam, write a comment explaining why the exam was denied and then click the "Deny" button.'
+      text: 'Om informationen inte är korrekt eller om det finns olämplig information i tentan, skriv en kommentar som förklarar varför tentan nekades och klicka sedan på "Neka tenta"-knappen.'
     },
     {
       image: checkbox,
-      text: 'The next exam will be displayed and you will repeat the same procedure.'
+      text: 'Nästa tenta kommer att visas och du upprepar samma procedur.'
+    },
+    {
+      image: checkbox,
+      text: 'Om det inte finns fler tentor att granska, kommer ett meddelande att visas för att informera dig om detta.'
+    },
+    {
+      image: checkbox,
+      text: 'Kom ihåg att ditt beslut är slutgiltigt. Var noga med att du har all nödvändig information innan du godkänner eller nekar en tenta.'
     }
   ];
+
 
   return (
     <div className={styles.info_section}>
@@ -29,7 +38,7 @@ const InfoSection = () => {
         <div className={styles.info_item} key={index}>
           <img src={item.image} alt={`Step ${index + 1}`} />
           <div className={styles.info_text}>
-            <p><strong>Step {index + 1}:</strong> {item.text}</p>
+            <p><strong>Steg {index + 1}:</strong> {item.text}</p>
           </div>
         </div>
       ))}
